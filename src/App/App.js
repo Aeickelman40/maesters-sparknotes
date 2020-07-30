f7feb217e09baab8be57beee44cebbfb016be679
Server <server@server.com>
2020-07-30 14:43:34 -0600

    Create option page component and homepage text

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import logo from '../images/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <h1>Sparknotes for Junior Maesters</h1>
+        <p>Don't have time to read volumes of old books in Kings Landing?</p>
+      </header>
+      
+      <button>Click here to enter!</button>
+    </div>
+  );
+}
+
+export default App;
