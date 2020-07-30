a0e4ce33d9c33d337dbff5e4573945434cd8aa93
Server <server@server.com>
2020-07-30 15:12:00 -0600

    Add react-dom to json, created apiCalls file, and begin routing from homepage

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-// import logo from '../images/logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Route, NavLink } from 'react-router-dom';
+import optionPage from '../optionPage/optionPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      <h1>Sparknotes for Junior Maesters</h1>
-        <p>Don't have time to read volumes of old books in Kings Landing?</p>
-      </header>
-      
-      <button>Click here to enter!</button>
-    </div>
-  );
+// import logo from '../images/logo.svg';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      houses: [],
+      characters: []
+      }
+    }
+
+  componentDidMount = async () => {
+    
+  }  
+
+  render() {
+    return (
+      <div className="App">
+         {/* <img src={logo} className="App-logo" alt="logo" />  */}
+        <h1>Sparknotes for Junior Maesters</h1>
+          <p>Don't have time to read volumes of old books in Kings Landing?</p>
+        <NavLink to={"/optionPage"}>
+          <button>Click here to enter!</button>
+        </NavLink>
+      <Route
+        path="/optionPage"
+        render={() => <optionPage />} />
+      </div>
+    );
+  }
 }
 
 export default App;
